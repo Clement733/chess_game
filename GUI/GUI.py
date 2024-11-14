122cb1136de6e36a6ed07b0fbd9dcd240734933b
clement cipriani <ciprianiclement@outlook.com>
2024-11-14 20:32:51 +0900

    Completed a basic GUI

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,8 +1,26 @@
 import tkinter as tk
+from tkinter.ttk import *
 
 g_w = tk.Tk()
 g_w.geometry("1080x680")
 g_w.title("Welcome to my chess game!♟️")
+
+def board():
+    pl_but.config(state="disable")
+    plcomp_but.config(state="disable")
+
+    bd = tk.Toplevel(g_w)
+    bd.geometry("1150x850")
+    bd.title("This is my try at a board window")
+
+    def on_close():
+        pl_but.config(state="active")
+        bd.destroy()
+
+        plcomp_but.config(state="active")
+        bd.destroy()
+
+    bd.protocol("WM_DELETE_WINDOW", on_close)
 
 pl_hdr = tk.StringVar()
 pl_hdr.set("Let's play some chess, baby!")
@@ -32,7 +50,8 @@ pl_but=tk.Button(g_w,
                  width=20,
                  font=("TimesNewRoman", 16, "bold", "italic"),
                  textvariable=players_but_txt,
-                 justify="center")
+                 justify="center",
+                 command=board)
 
 pl_but.pack()
 
@@ -45,7 +64,8 @@ plcomp_but=tk.Button(g_w,
                      width=20,
                      font=("TimesNewRoman", 16, "bold", "italic"),
                      textvariable=plcomp_but_txt,
-                     justify="center")
+                     justify="center",
+                     command=board)
 
 plcomp_but.pack(anchor="s")
 
